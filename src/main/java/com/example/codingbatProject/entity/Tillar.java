fcb25ea02ef8af68db6e472bb43162bdd7c62331
Akmalbek2002 <axrorovakmal256@gmail.com>
2022-11-23 11:00:07 +0500

    jadval yaratildi

--- src/main/java/com/example/codingbatProject/entity/Tillar.java
+++ src/main/java/com/example/codingbatProject/entity/Tillar.java
@@ -1,4 +1,14 @@
 package com.example.codingbatProject.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
 public class Tillar {
 }
