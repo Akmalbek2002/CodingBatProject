0ac45f6992527a7a7828db0cc8cb28d458c4a4f1
Akmalbek2002 <axrorovakmal256@gmail.com>
2022-11-23 10:51:54 +0500

    coding bat

---
+++ src/main/java/com/example/codingbatProject/CodingbatProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.codingbatProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingbatProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodingbatProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/codingbatProject/CodingbatProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.codingbatProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodingbatProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
