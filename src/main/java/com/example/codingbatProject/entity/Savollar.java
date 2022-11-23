737e68b18abfa2d32d909fba07e51752d410eb5b
Akmalbek2002 <axrorovakmal256@gmail.com>
2022-11-23 22:19:49 +0500

    jadval yaratildi

--- src/main/java/com/example/codingbatProject/entity/Savollar.java
+++ src/main/java/com/example/codingbatProject/entity/Savollar.java
@@ -4,10 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -17,10 +14,15 @@ public class Savollar {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+    @Column(nullable = false)
     private String savolNomi;
+    @Column(nullable = false)
     private String savolMatni;
+    @Column(nullable = false)
     private String yechimNamunasi;
+    @Column(nullable = false)
     private String funksiya;
+    @Column(nullable = false)
     private String namunasi;
 
 }
